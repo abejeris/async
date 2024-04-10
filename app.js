fc0d970dd57d2fcbfde2da73499d447a4099cd4e
Audrius Bejeris <audriusbejeris@gmail.com>
2024-04-10 19:09:27 +0300

    added parallel and sequential async functions

--- app.js
+++ app.js
@@ -52,13 +52,48 @@
 
 // //with try catch block--------------------------------
 
-async function getPlanets() {
-	try {
-		const res = await axios.get('https://swapi.dev/api/planets');
-		console.log(res.data);
-	} catch (e) {
-		console.log('error is: ', e);
-	}
-}
+// async function getPlanets() {
+// 	try {
+// 		const res = await axios.get('https://swapi.dev/api/planets');
+// 		console.log(res.data);
+// 	} catch (e) {
+// 		console.log('error is: ', e);
+// 	}
+// }
 
-getPlanets();
+// getPlanets();
+
+// ----------------------------------------------------------------------------
+// multiple await examples
+// sequential example
+
+// async function getPokemon() {
+// 	try {
+// 		const res1 = await axios.get('https://pokeapi.co/api/v2/pokemon/1');
+// 		const res2 = await axios.get('https://pokeapi.co/api/v2/pokemon/2');
+// 		const res3 = await axios.get('https://pokeapi.co/api/v2/pokemon/3');
+// 		console.log(res1.data.name);
+// 		console.log(res2.data.name);
+// 		console.log(res3.data.name);
+// 	} catch (e) {
+// 		console.log(e);
+// 	}
+// }
+// getPokemon();
+
+// -----------------------------------------------------------------------
+// parallel example
+
+async function getPokemon() {
+	const res1 = axios.get('https://pokeapi.co/api/v2/pokemon/1');
+	const res2 = axios.get('https://pokeapi.co/api/v2/pokemon/2');
+	const res3 = axios.get('https://pokeapi.co/api/v2/pokemon/3');
+	console.log(res1);
+	const poke1 = await res1;
+	const poke2 = await res2;
+	const poke3 = await res3;
+	console.log(poke1.data);
+	console.log(poke2.data);
+	console.log(poke3.data);
+}
+getPokemon();
