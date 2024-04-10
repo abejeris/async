ea03de0329d4f61b067e2647ab3b4a95cc573a04
Audrius Bejeris <audriusbejeris@gmail.com>
2024-04-10 19:17:59 +0300

    added Promise.all example

--- app.js
+++ app.js
@@ -84,16 +84,34 @@
 // -----------------------------------------------------------------------
 // parallel example
 
+// async function getPokemon() {
+// 	const res1 = axios.get('https://pokeapi.co/api/v2/pokemon/1');
+// 	const res2 = axios.get('https://pokeapi.co/api/v2/pokemon/2');
+// 	const res3 = axios.get('https://pokeapi.co/api/v2/pokemon/3');
+// 	console.log(res1);
+// 	const poke1 = await res1;
+// 	const poke2 = await res2;
+// 	const poke3 = await res3;
+// 	console.log(poke1.data);
+// 	console.log(poke2.data);
+// 	console.log(poke3.data);
+// }
+// getPokemon();
+
+// ---------------------------------------------------------------------------------
+// Promise.all example
+
 async function getPokemon() {
-	const res1 = axios.get('https://pokeapi.co/api/v2/pokemon/1');
-	const res2 = axios.get('https://pokeapi.co/api/v2/pokemon/2');
-	const res3 = axios.get('https://pokeapi.co/api/v2/pokemon/3');
-	console.log(res1);
-	const poke1 = await res1;
-	const poke2 = await res2;
-	const poke3 = await res3;
-	console.log(poke1.data);
-	console.log(poke2.data);
-	console.log(poke3.data);
+	const prom1 = axios.get('http://pokeapi.co/api/v2/pokemon/1');
+	const prom2 = axios.get('http://pokeapi.co/api/v2/pokemon/2');
+	const prom3 = axios.get('http://pokeapi.co/api/v2/pokemon/3');
+	const results = await Promise.all([prom1, prom2, prom3]);
+	printPokemon(results);
 }
+function printPokemon(results) {
+	for (let pokemon of results) {
+		console.log(pokemon.data.name);
+	}
+}
+
 getPokemon();
